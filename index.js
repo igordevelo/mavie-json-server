85a7cb5dd7a42a13c51136205a6a14e74280c398
Igor Łuczko <tymekluczko@gmail.com>
2021-10-20 19:50:09 +0200

    added endpoints for home, market, content including single routes

--- index.js
+++ index.js
@@ -75,6 +75,125 @@ server.post("/auth/verify-code", (req, res) => {
   res.status(200).jsonp({ status: "ok", data: userFound });
 });
 
+// Add custom routes before JSON Server router
+server.get("/screen/home", (req, res) => {
+  const response = {
+    status: "ok",
+    data: {
+      notification: "Your employer just deposited $10 in your account.",
+      balance: 255,
+      saved: db.saved,
+      market: db.products,
+      content: db.posts,
+    },
+  };
+
+  res.status(200).jsonp(response);
+});
+
+server.get("/screen/content", (req, res) => {
+  const posts = db.posts;
+  const response = {
+    status: "ok",
+    data: { posts },
+  };
+
+  res.status(200).jsonp(response);
+});
+
+server.get("/screen/market", (req, res) => {
+  const response = {
+    status: "ok",
+    data: {
+      header: [
+        {
+          id: 1,
+          title: "MOLEKULE",
+          body: "A new professional-grade air purifier",
+          earn: { type: "percentage", amount: 8 },
+          image:
+            "https://atmz-develo.s3.eu-central-1.amazonaws.com/imagemarketheader.png",
+        },
+        {
+          id: 2,
+          title: "MOLEKULE 2",
+          body: "2 A new professional-grade air purifier",
+          earn: { type: "percentage", amount: 5 },
+          image:
+            "https://atmz-develo.s3.eu-central-1.amazonaws.com/imagemarketheader.png",
+        },
+      ],
+      saved: db.saved,
+      new: db.new,
+      streams: [
+        { id: 1, title: "Body", items: db.products },
+        { id: 2, title: "Mind", items: db.products[1] },
+      ],
+    },
+  };
+
+  res.status(200).jsonp(response);
+});
+
+server.get("/screen/product/:productId", (req, res) => {
+  console.log(req.params.productId);
+  const productId = parseInt(req.params.productId, 10);
+
+  const product = db.products.find((product) => product.id === productId);
+
+  if (product == null) {
+    res.status(400).jsonp({ status: "error", error: "product not existing" });
+  }
+  const response = {
+    status: "ok",
+    data: {
+      product,
+    },
+  };
+
+  res.status(200).jsonp(response);
+});
+
+server.get("/screen/service/:serviceId", (req, res) => {
+  console.log(req.params.serviceId);
+  const serviceId = parseInt(req.params.serviceId, 10);
+
+  const service = db.services.find((service) => service.id === serviceId);
+
+  if (service == null) {
+    res.status(400).jsonp({ status: "error", error: "service not existing" });
+  }
+  const response = {
+    status: "ok",
+    data: {
+      service,
+    },
+  };
+
+  res.status(200).jsonp(response);
+});
+
+server.get("/screen/content/:contentId", (req, res) => {
+  console.log(req.params.contentId);
+  const contentId = parseInt(req.params.contentId, 10);
+
+  const content = db.posts.find((content) => content.id === contentId);
+
+  if (content == null) {
+    res.status(400).jsonp({ status: "error", error: "content not existing" });
+  }
+
+  content.related = [db.posts[1]];
+  const response = {
+    status: "ok",
+    data: {
+      content,
+    },
+  };
+
+  res.status(200).jsonp(response);
+});
+
 server.use(router);
 
 server.listen(port);
